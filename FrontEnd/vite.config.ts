f40c2f5ee3cde96d28aeef58d13001793cddde4a
Weslley Silva <weslleysilva.swe@gmail.com>
2024-08-23 14:09:25 -0300

    chore: Update npm dependencies and add FontAwesome icons to Home page

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import "dotenv/config";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@types": path.resolve(__dirname, "src/types"),
+    },
+  },
+});
